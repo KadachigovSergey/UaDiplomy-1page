e8179c5fe0e7f54414f74851f1ac33b0d1cdcc47
MrKadachigov <kadachigovs@gmail.com>
2016-12-03 00:10:54 +0200

    save work

--- dist/js/plugin.js
+++ dist/js/plugin.js
@@ -1,3 +1,14 @@
-/**
- * Created by MrKadachigov on 29.11.2016.
- */
+;(function ($){
+    $(window).on('load', function(){
+        // $('select').styler();
+
+
+        var sidebarBtn = $('.mobile-btn');
+        var sidebar = $('.list-for-upper');
+        sidebarBtn.on('click', function (){
+            sidebarBtn.toggleClass('open');
+            sidebar.toggleClass('open');
+        });
+    });
+
+})(jQuery);
